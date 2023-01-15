2e01bddf03dabeba6ab156b29df0e59eec343cdc
Adi Makkar <adimakkar.ca@gmail.com>
2023-01-15 11:38:25 -0500

    Jest file added

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    isolatedModules: true,
+    globals: {
+      'testMatch': [
+      "<rootDir>/src/**/*.test.ts"
+    ]
+    }
+  };
+  
