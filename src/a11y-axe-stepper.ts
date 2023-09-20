62658e5e1ad3da704c47c0f8bf87ccc7f48f303f
Vid <vid_github@zooid.org>
2023-09-20 18:56:15 +0000

    check for storage at runtime

--- src/a11y-axe-stepper.ts
+++ src/a11y-axe-stepper.ts
@@ -1,27 +1,27 @@
 import { AStepper, TWorld, TNamed, IHasOptions, OK } from '@haibun/core/build/lib/defs.js';
-import { actionNotOK, actionOK, findStepper, maybeFindStepperFromOption, stringOrError } from '@haibun/core/build/lib/util/index.js';
+import { actionNotOK, actionOK, findStepper, findStepperFromOption, stringOrError } from '@haibun/core/build/lib/util/index.js';
 import { chromium, Page } from 'playwright';
-import { evalSeverity, getReport } from './lib/a11y-axe.js';
-import { generateAxeReport } from './lib/report.js';
+import { evalSeverity, getAxeBrowserResult } from './lib/a11y-axe.js';
+import { generateHTMLAxeReportFromStepReport, generateHTMLAxeReportFromBrowserResult } from './lib/report.js';
 import { AStorage } from '@haibun/domain-storage/build/AStorage.js';
 import { EMediaTypes } from '@haibun/domain-storage/build/domain-storage.js';
 
-const STORAGE = 'STORAGE';
-
 type TGetsPage = { getPage: () => Promise<Page> };
+
 class A11yStepper extends AStepper implements IHasOptions {
+  static STORAGE = 'STORAGE';
   options = {
-    [STORAGE]: {
+    [A11yStepper.STORAGE]: {
       desc: 'Storage for results',
       parse: (input: string) => stringOrError(input),
     },
   };
   pageGetter?: TGetsPage;
-  storage?: AStorage;
-  setWorld(world: TWorld, steppers: AStepper[]) {
-    super.setWorld(world, steppers);
+  steppers: AStepper[] = [];
+  async setWorld(world: TWorld, steppers: AStepper[]) {
+    await super.setWorld(world, steppers);
     this.pageGetter = findStepper<TGetsPage>(steppers, 'WebPlaywright');
-    this.storage = maybeFindStepperFromOption(steppers, this, world.extraOptions, STORAGE);
+    this.steppers = steppers;
   }
 
   steps = {
@@ -33,55 +33,49 @@ class A11yStepper extends AStepper implements IHasOptions {
           return actionNotOK(`no page in runtime`)
         }
 
-        try {
-          const axeReport = await getReport(page);
-          const evaluation = evalSeverity(axeReport, { serious: parseInt(serious!) || 0, moderate: parseInt(moderate!) || 0 });
-          if (evaluation.ok) {
-            return actionOK({ axeSuccess: { summary: 'conditions', details: { axeReport, res: evaluation } } });
-          }
-          const message = `not acceptable`;
-          return actionNotOK(message, { topics: { axeFailure: { summary: message, details: { axeReport, res: evaluation } } } });
-        } catch (e) {
-          const { message } = { message: 'test' };
-          return actionNotOK(message, { topics: { exception: { summary: message, details: e } } });
-        }
+        return await this.checkA11y(page, serious!, moderate!);
+
       }
     },
     checkA11yWithUri: {
+      //             page at http://localhost:8123/static/passes.html is accessible accepting serious 0 and moderate 2
       gwta: `page at {uri} is accessible accepting serious {serious} and moderate {moderate}`,
       action: async ({ uri, serious, moderate }: TNamed) => {
         const browser = await chromium.launch();
         const page: Page = await browser.newPage();
         await page.goto(uri!);
 
-        try {
-          const axeReport = await getReport(page);
-          const evaluation = evalSeverity(axeReport, { serious: parseInt(serious!) || 0, moderate: parseInt(moderate!) || 0 });
-          if (evaluation.ok) {
-            return actionOK({ axeSuccess: { summary: 'acceptable', details: { axeReport, res: evaluation } } });
-          }
-          const message = `not acceptable: ${axeReport}`;
-          return actionNotOK(message);
-        } catch (e) {
-          const { message } = { message: 'test' };
-          return actionNotOK(message, { topics: { exception: { summary: message, details: e } } });
-        } finally {
-          page.close();
-          browser.close();
-        }
+        const result = await this.checkA11y(page, serious!, moderate!);
+
+        // page.close();
+        // browser.close();
+        return result;
       }
     },
     generateHTMLRereport: {
       gwta: `generate HTML report from {source} to {dest}`,
       action: async ({ source, dest }: TNamed) => {
-        if (!this.storage) {
-          return actionNotOK('Storage option is not configured');
-        }
-        const json = JSON.parse(this.storage.readFile(source!));
-        const report = generateAxeReport(json);
-        this.storage.writeFile(dest!, report, EMediaTypes.html);
+        const storage = findStepperFromOption<AStorage>(this.steppers, this, this.getWorld().extraOptions, A11yStepper.STORAGE);
+        const json = JSON.parse(storage.readFile(source!));
+        const report = generateHTMLAxeReportFromStepReport(json);
+        storage.writeFile(dest!, report, EMediaTypes.html);
         return OK;
       }
+    }
+  }
+  async checkA11y(page: Page, serious: string, moderate: string) {
+    try {
+      const axeReport = await getAxeBrowserResult(page);
+      const evaluation = evalSeverity(axeReport, { serious: parseInt(serious!) || 0, moderate: parseInt(moderate!) || 0 });
+      if (evaluation.ok) {
+        return actionOK({ axeSuccess: { summary: 'conditions', details: { axeReport, res: evaluation } } });
+      }
+      const message = `not acceptable`;
+      const html = generateHTMLAxeReportFromBrowserResult(axeReport);
+      return actionNotOK(message, { topics: { axeFailure: { summary: message, details: { axeReport, res: evaluation } } } });
+    } catch (e) {
+      const { message } = { message: 'test' };
+      return actionNotOK(message, { topics: { exception: { summary: message, details: e } } });
     }
   }
 }
