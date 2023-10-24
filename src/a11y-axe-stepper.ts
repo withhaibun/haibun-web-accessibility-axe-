0b947956a891d8931d4e57e26d9d09a3163a5bea
Vid <vid_github@zooid.org>
2023-10-24 15:35:07 -0400

    adjust artifact

--- src/a11y-axe-stepper.ts
+++ src/a11y-axe-stepper.ts
@@ -1,11 +1,11 @@
-import { AStepper, TWorld, TNamed, IHasOptions, OK } from '@haibun/core/build/lib/defs.js';
+import { AStepper, TWorld, TNamed, IHasOptions, OK, TVStep } from '@haibun/core/build/lib/defs.js';
 import { actionNotOK, findStepper, findStepperFromOption, stringOrError } from '@haibun/core/build/lib/util/index.js';
 import { chromium, Page } from 'playwright';
 import { evalSeverity, getAxeBrowserResult } from './lib/a11y-axe.js';
 import { generateHTMLAxeReportFromStepReport, generateHTMLAxeReportFromBrowserResult } from './lib/report.js';
 import { AStorage } from '@haibun/domain-storage/build/AStorage.js';
 import { EMediaTypes } from '@haibun/domain-storage/build/domain-storage.js';
-import { TArtifact, TMessageContext } from '@haibun/core/build/lib/interfaces/logger.js';
+import { TArtifact, TArtifactMessageContext, TMessageContext } from '@haibun/core/build/lib/interfaces/logger.js';
 
 type TGetsPage = { getPage: () => Promise<Page> };
 
@@ -28,22 +28,22 @@ class A11yStepper extends AStepper implements IHasOptions {
   steps = {
     checkA11yRuntime: {
       gwta: `page is accessible accepting serious {serious} and moderate {moderate}`,
-      action: async ({ serious, moderate }: TNamed) => {
+      action: async ({ serious, moderate }: TNamed, step: TVStep) => {
         const page = await this.pageGetter?.getPage();
         if (!page) {
           return actionNotOK(`no page in runtime`);
         }
-        return await this.checkA11y(page, serious!, moderate!);
+        return await this.checkA11y(page, serious!, moderate!, step.seq, 'action');
       },
     },
     checkA11yWithUri: {
       gwta: `page at {uri} is accessible accepting serious {serious} and moderate {moderate}`,
-      action: async ({ uri, serious, moderate }: TNamed) => {
+      action: async ({ uri, serious, moderate }: TNamed, step: TVStep) => {
         const browser = await chromium.launch();
         const page: Page = await browser.newPage();
         await page.goto(uri!);
 
-        const result = await this.checkA11y(page, serious!, moderate!);
+        const result = await this.checkA11y(page, serious!, moderate!, step.seq, 'action');
 
         // page.close();
         // browser.close();
@@ -61,7 +61,7 @@ class A11yStepper extends AStepper implements IHasOptions {
       },
     },
   };
-  async checkA11y(page: Page, serious: string, moderate: string) {
+  async checkA11y(page: Page, serious: string, moderate: string, seq: number, stage: string) {
     try {
       const axeReport = await getAxeBrowserResult(page);
       const evaluation = evalSeverity(axeReport, {
@@ -74,9 +74,7 @@ class A11yStepper extends AStepper implements IHasOptions {
       }
       const message = `not acceptable`;
       const html = generateHTMLAxeReportFromBrowserResult(axeReport);
-      this.getWorld().logger.error(message, <TMessageContext>{
-        artifact: <TArtifact>{ type: 'html', content: html, event: 'failure' },
-      });
+      this.getWorld().logger.error(message, <TArtifactMessageContext>{ topic: { seq: seq, event: 'failure', stage }, artifact: { type: 'html', content: html, }, tag: this.getWorld().tag });
       return actionNotOK(message, {
         topics: {
           axeFailure: {
